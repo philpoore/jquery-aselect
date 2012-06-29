6f3e2b20b2445ce9d3bf9ca4af7c76844f27d270
Andy <andy@kaweb.co.uk>
2012-06-29 10:31:57 +0100

    Firefox bugs  /  Comments Added

--- jquery-aselect.js
+++ jquery-aselect.js
@@ -1,22 +1,22 @@
 /*
-**  
-	                     .__                 __   
-	_____    ______ ____ |  |   ____   _____/  |_ 
+**
+	                     .__                 __
+	_____    ______ ____ |  |   ____   _____/  |_
 	\__  \  /  ___// __ \|  | _/ __ \_/ ___\   __\
-	 / __ \_\___ \\  ___/|  |_\  ___/\  \___|  |  
-	(____  /____  >\___  >____/\___  >\___  >__|  
-	     \/     \/     \/          \/     \/     
-	     
+	 / __ \_\___ \\  ___/|  |_\  ___/\  \___|  |
+	(____  /____  >\___  >____/\___  >\___  >__|
+	     \/     \/     \/          \/     \/
+
 	     			jQuery plugin
-	     			
+
 	     						- andy jones
 	     						- phil poore
 	     						- little jase
 
 
 	About - Cross browser support for nice <select></select> tags
-	
-	
+
+
 	Useage:
 		$(function(){
 			$('select').aselect();
@@ -24,11 +24,11 @@
 */
 (function ($){
 	$.fn.aselect = function (options)
-	{	 
-		var defaults = {}  
-		var options = $.extend(defaults, options);  
+	{
+		var defaults = {}
+		var options = $.extend(defaults, options);
 		return this.each(function (){
-		 	
+
 		$(this).not('.jquery-aselect').each(function(){
 		 		var width = parseInt(options.width) || $(this).outerWidth();
 	 			$(this).attr('autocomplete', 'off').addClass('jquery-aselect')
@@ -37,7 +37,7 @@
 	 			).before(
 	 				$(document.createElement('span'))
 	 				.addClass('jquery-aselect-selected-text').width(width).css('background-position-x',(width-499)+'px')
-	 			).css({width: width - 11 + 'px'});
+	 			).css({width: width - 0 + 'px'});
 	 		}).change(function(){
 	 			var selectedText = $(this).find(':selected').text();
 	 			$(this).prev().text(selectedText);
